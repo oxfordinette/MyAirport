808e1f9c8b310fe69f3db7ba08dec54688b4e81b
Sandra BARAKET <sandra.baraket@edu.ece.fr>
2020-02-21 14:36:19 +0100

    creation of the solution

---
+++ MyAirport/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MASB.MyAirport.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
